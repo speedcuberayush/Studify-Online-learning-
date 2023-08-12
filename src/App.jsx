1ee85152458a9f97f0876acbcbe64d40e1553d58
Ayush Mishra <msdakm3334@gmail.com>
2023-08-12 23:40:37 +0530

    session 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ProjectPreloader from "./Components/Projectpreloader.jsx";
 import PageTransition from "./Components/PageTransition.jsx";
 import Landingpage from "./Components/Landingpage.jsx";
+import Error404 from "./Components/Error404.jsx";
 function App() {
   return (
     <div className="app">
       {/* <ProjectPreloader /> */}
-      <Landingpage />
+      {/* <Landingpage /> */}
       {/* <PageTransition /> */}
+      <Error404 />
     </div>
   );
 }
