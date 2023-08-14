9f5d6611d6c9fb16e6d1e26c0ba19b42b034ae1c
Ayush Mishra <msdakm3334@gmail.com>
2023-08-14 23:50:35 +0530

    session 6

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -5,10 +5,31 @@ import React from "react";
 export default function Landing() {
   return (
     <div className="landingpage">
-      <div className="part1">header</div>
+      <div className="part1">
+        <Logo />
+      </div>
       <div className="part2">
         {/* typing text effects */}
-        <Logo />
+        <p>WELCOME TO STUDIFY</p>
+        <div class="animate-contain">
+          <div class="animated-text">
+            <span>Learning Reimagined</span>
+            <span>Empowering Minds</span>
+            <span>Enriching Futures</span>
+            <span>A campus connection & community hub</span>
+            <span>Your Gateway to Tech Brilliance</span>
+          </div>
+        </div>
+      </div>
+      <div className="part3">
+        <div className="container">
+          <div className="btn">
+            <a href="#">LOGIN/ SIGNUP</a>
+          </div>
+          <div className="btn">
+            <a href="#">DOWNLOAD </a>
+          </div>
+        </div>
       </div>
       <div className="part4">
         {/* footer */}
