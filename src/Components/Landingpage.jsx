68733c55435e6e9aab453a17a7b14df36eb23863
Ayush Mishra <msdakm3334@gmail.com>
2023-08-14 18:13:31 +0530

    session 5

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import "./styles/Landingpage.css";
 import Logo from "./Logo.jsx";
+import React from "react";
+
 export default function Landing() {
   return (
     <div className="landingpage">
@@ -8,20 +9,10 @@ export default function Landing() {
       <div className="part2">
         {/* typing text effects */}
         <Logo />
-        <ul>
-          <li>Learning Reimagined: Welcome to STUDIFY</li>
-          <li>Empowering Minds, Enriching Futures</li>
-          <li>A campus connection and community platform</li>
-          <li>Your Gateway to Tech Brilliance.</li>
-        </ul>
       </div>
-      <div className="part3">buttons</div>
       <div className="part4">
         {/* footer */}
-        <ul>
-          <li>social medias</li>
-          <li> ©2023 STUDIFY™ All Rights Reserved.</li>
-        </ul>
+        ©2023 STUDIFY™ All Rights Reserved
       </div>
     </div>
   );
