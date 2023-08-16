b107e77250781896c603caa3224f364f651ca32c
Ayush Mishra <msdakm3334@gmail.com>
2023-08-16 22:14:10 +0530

    session 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import React from "react";
-import ProjectPreloader from "./Components/Projectpreloader.jsx";
-import PageTransition from "./Components/PageTransition.jsx";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Landingpage from "./Components/Landingpage.jsx";
 import Authenticationpage from "./Components/Authenticationpage.jsx";
+import PageTransition from "./Components/PageTransition.jsx";
 import Error404 from "./Components/Error404.jsx";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
+import ProjectPreloader from "./Components/Projectpreloader.jsx";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a 7-second loading process
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="app">
+        {isLoading && <ProjectPreloader />}
         <Routes>
-          {/* <ProjectPreloader /> */}
-          <Route path={"/"} element={<Landingpage />}></Route>
-          <Route path={"/login"} element={<Authenticationpage />}></Route>
-          <Route path={"/transition"} element={<PageTransition />}></Route>
-          <Route path={"*"} element={<Error404 />}></Route>
-
+          <Route path={"/"} element={<Landingpage />} />
+          <Route path={"/login"} element={<Authenticationpage />} />
+          <Route path={"/transition"} element={<PageTransition />} />
+          <Route path={"*"} element={<Error404 />} />
         </Routes>
+        {/* <Navbar /> */}
       </div>
     </BrowserRouter>
   );
